0e519ca89d7578ea39c166314c299070b4c0f973
joseph-birara <josephbirara92@gmail.com>
2022-04-26 22:28:01 +0300

    footer component added

---
+++ restaurant_website/src/Components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.css";
+import Wave from "../../Images/wave.png";
+import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Twitter from "@iconscout/react-unicons/icons/uil-twitter";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <img src={Wave} alt="" style={{ width: "100%" }} />
+      <div className="f-content">
+        <span>restaurant@gmail.com</span>
+        <div className="f-icons">
+          <Insta color="white" size={"3rem"} />
+          <Facebook color="white" size={"3rem"} />
+          <Twitter color="white" size={"3rem"} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
